19582f5c7af1f70e4ca041ffdd0b7ade268e5a3e
Mats Saaren <167091453+matssaaren@users.noreply.github.com>
2025-06-15 03:57:04 +0300

    Update Layout.js

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import './Layout.css'
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { useLayoutEffect } from "react";
 
 const Layout = () => {
   
