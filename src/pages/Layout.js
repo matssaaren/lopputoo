4de37a71de4e6e1799ff539b40faf776faceb36a
matssaaren <167091453+matssaaren@users.noreply.github.com>
2025-06-15 04:02:57 +0300

    Fix scroll to top on page change

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -2,7 +2,6 @@ import { Outlet } from "react-router-dom";
 import './Layout.css'
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { useLayoutEffect } from "react";
 
 const Layout = () => {
 
